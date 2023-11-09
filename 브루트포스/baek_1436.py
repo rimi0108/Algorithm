b389ae957637090a701d9004cb09e91d97ab700c
hyerchoi <hyerimc858@gmail.com>
2023-11-09 14:17:18 +0900

    231109

---
+++ 브루트포스/baek_1436.py
@@ -0,0 +1,17 @@
+# 영화감독 숌
+
+# 1 <= n <= 10,000 ...?
+n = int(input())
+
+
+i = 665
+j = 0
+
+while 1:
+    i += 1
+    if '666' in str(i):
+        j += 1
+        if j == n:
+            break
+
+print(i)
